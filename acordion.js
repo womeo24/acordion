4fd9228f084c9a32c387a04462afebff00b5e664
womeo24 <womeo24@gmail.com>
2023-10-25 12:56:35 +0100

    update

--- acordion.js
+++ acordion.js
@@ -12,3 +12,37 @@ for (let i = 0; i < accordions.length; i++) {
         }
     });
 }
+
+
+// array
+// const fruits = [ 'bannana', 'pawpaw',  'apple', 'orange', 'mango']
+// let flen = fruits.length;
+// let text = '<ul>';
+// for (let i = 0; i < flen; i++) {
+//     text += '<li>' + fruits[i] + '<li/>'
+// }
+// text += '</ul>'
+
+
+// // array thn with element
+// // fruits[0]= 'gt'
+// // fruits[1]= 'toyota';
+// //  fruits[2]= 'honda';
+//  document.getElementById("demo").innerHTML = fruits.toString();
+//  console.log('fruits', fruits)
+
+function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    btn = document.getElementsByClassName("btn");
+    for (i = 0; i < btn.length; i++) {
+        btn[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+}
+
+    document.getElementById("defaultOpen").click();
